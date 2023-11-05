ea60a4d61153cf24acebade7123ae9823f4b351e
Ta David Yu <ta.yu.gamedev@gmail.com>
2023-11-05 01:42:16 +0100

    Added test for workbook parsing & company data parser

--- CalculatorTests/ExtensionTest.cs
+++ CalculatorTests/ExtensionTest.cs
@@ -1,4 +1,7 @@
-﻿using RelativeLinkCalculator.Extensions;
+﻿using NanoXLSX;
+using RelativeLinkCalculator;
+using RelativeLinkCalculator.Extensions;
+using System.Text;
 
 namespace CalculatorTests
 {
@@ -49,10 +52,10 @@ namespace CalculatorTests
 				collection[i] = extractedRelativeName;
 			}
 
-			Assert.AreEqual(collection[0], "陳秀雄");
-			Assert.AreEqual(collection[1], "羅元佑");
-			Assert.AreEqual(collection[2], "羅才仁");
-			Assert.AreEqual(collection[3], "陳涵琦");
+			Assert.AreEqual("陳秀雄", collection[0]);
+			Assert.AreEqual("羅元佑", collection[1]);
+			Assert.AreEqual("羅才仁", collection[2]);
+			Assert.AreEqual("陳涵琦", collection[3]);
 		}
 
 		[TestMethod]
@@ -69,10 +72,59 @@ namespace CalculatorTests
 				collection[i] = extractedRelativeName;
 			}
 
-			Assert.AreEqual(collection[0], "羅才仁");
-			Assert.AreEqual(collection[1], "陳秀雄");
-			Assert.AreEqual(collection[2], "陳榮華");
-			Assert.AreEqual(collection[3], "羅銘釔");
+			Assert.AreEqual("羅才仁", collection[0]);
+			Assert.AreEqual("陳秀雄", collection[1]);
+			Assert.AreEqual("陳榮華", collection[2]);
+			Assert.AreEqual("羅銘釔", collection[3]);
+		}
+
+		[TestMethod]
+		public void ParseWorkbook1()
+		{
+			System.Console.OutputEncoding = Encoding.Unicode;
+			string fileName = "TestFiles//2022橡膠工業.xlsx";
+
+			Workbook workbook = Workbook.Load(fileName);
+			var companyDataCollection = WorkbookParser.ParseCompanyDataWorkbook(workbook);
+
+			int numberOfCompanies = companyDataCollection.Count;
+			Assert.AreEqual(11, numberOfCompanies);
+
+			Assert.AreEqual("2101 南港",		companyDataCollection[0].Name);
+			Assert.AreEqual("2102 泰豐",		companyDataCollection[1].Name);
+			Assert.AreEqual("2103 台橡",		companyDataCollection[2].Name);
+			Assert.AreEqual("2104 國際中橡", companyDataCollection[3].Name);
+			Assert.AreEqual("2105 正新",		companyDataCollection[4].Name);
+			Assert.AreEqual("2106 建大",		companyDataCollection[5].Name);
+			Assert.AreEqual("2107 厚生",		companyDataCollection[6].Name);
+			Assert.AreEqual("2108 南帝",		companyDataCollection[7].Name);
+			Assert.AreEqual("2109 華豐",		companyDataCollection[8].Name);
+			Assert.AreEqual("2114 鑫永銓",	companyDataCollection[9].Name);
+			Assert.AreEqual("6582 申豐",		companyDataCollection[10].Name);
+
+			Assert.AreEqual(0, companyDataCollection[0].RelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[1].RelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[2].RelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[3].RelativeLinkCount);
+			Assert.AreEqual(31, companyDataCollection[4].RelativeLinkCount);
+			Assert.AreEqual(26, companyDataCollection[5].RelativeLinkCount);
+			Assert.AreEqual(10, companyDataCollection[6].RelativeLinkCount);
+			Assert.AreEqual(7, companyDataCollection[7].RelativeLinkCount);
+			Assert.AreEqual(4, companyDataCollection[8].RelativeLinkCount);
+			Assert.AreEqual(8, companyDataCollection[9].RelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[10].RelativeLinkCount);
+
+			Assert.AreEqual(0, companyDataCollection[0].BoardRelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[1].BoardRelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[2].BoardRelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[3].BoardRelativeLinkCount);
+			Assert.AreEqual(2, companyDataCollection[4].BoardRelativeLinkCount);
+			Assert.AreEqual(11, companyDataCollection[5].BoardRelativeLinkCount);
+			Assert.AreEqual(3, companyDataCollection[6].BoardRelativeLinkCount);
+			Assert.AreEqual(7, companyDataCollection[7].BoardRelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[8].BoardRelativeLinkCount);
+			Assert.AreEqual(1, companyDataCollection[9].BoardRelativeLinkCount);
+			Assert.AreEqual(0, companyDataCollection[10].BoardRelativeLinkCount);
 		}
 	}
 }
--- RelativeLinkCalculator/Program.cs
+++ RelativeLinkCalculator/Program.cs
@@ -8,203 +8,15 @@ namespace RelativeLinkCalculator // Note: actual namespace depends on the projec
 {
 	internal class Program
 	{
-		const int kCompanyCodeColumn = 0;			// 公司代碼
-		const int kDateColumn = 1;					// 年月日
-		const int kStockHolderNameColumn = 2;		// 持股人姓名
-		const int kHolderNameColumn = 3;			// 持有人姓名
-		const int kPositionColumn = 4;				// 身分別
-		const int kRelativeOrSummaryColumn = 5;		// 親屬/總說明
-
-		static readonly Regex kNotRelativeColumnPattern = new Regex(@"集團");
-
-		class CompanyData
-		{
-			public string Name;
-
-			public int RelativeLinkCount;
-			public int TotalPositionCount;
-			public int IndependentDirectorCount;
-
-			public int BoardRelativeLinkCount;
-			public int BoardPositionCount;
-
-			/// <summary>
-			/// Holder (member) name -> A list of the names of the member relatives
-			/// </summary>
-			public Dictionary<string, HashSet<string>> RelativeMap = new Dictionary<string, HashSet<string>>();
-			public HashSet<string> BoardAndIndependentDirectors = new HashSet<string>();
-		}
-
 		static void Main(string[] args)
 		{
 			System.Console.OutputEncoding = Encoding.Unicode;
 			string fileName = "Input//2022橡膠工業.xlsx";
+
 			Workbook workbook = Workbook.Load(fileName);
-			parseWorkbook(workbook);
-		}
+			var companyDataCollection = WorkbookParser.ParseCompanyDataWorkbook(workbook);
 
-		private static void parseWorkbook(Workbook workbook)
-		{
-			Worksheet sheet = workbook.Worksheets[0];
-
-			IReadOnlyList<Cell> columnA = sheet.GetColumn("A");
-			int numberOfEntries = columnA.Count;
-
-			System.Console.Write($"Number Of Entries: {numberOfEntries}\n\n");
-
-			List<CompanyData> compantDataCollection = new List<CompanyData>();
-			CompanyData currentCompanyData = new CompanyData();
-
-			// We start with index 1, because the first row is always the title of each column.
-			for (int rowIndex = 1; rowIndex < numberOfEntries; rowIndex++)
-			{
-				Address companyCodeAddress = new Address(column: kCompanyCodeColumn, row: rowIndex);
-				Address holderNameAddress = new Address(column: kHolderNameColumn, row: rowIndex);
-				Address positionAddress = new Address(column: kPositionColumn, row: rowIndex);
-				Address relativeAddress = new Address(column: kRelativeOrSummaryColumn, row: rowIndex);
-
-				bool isSumUpRow = !sheet.TryGetCell(holderNameAddress, out Cell holderNameCell) || holderNameCell.DataType != Cell.CellType.STRING;
-				if (isSumUpRow)
-				{
-					// If holder name cell cannot be found), it means the row is a sum-up row (i.e., not an legal individual);
-					// therefore we want to skip these rows.
-					continue;
-				}
-
-				string holderName = holderNameCell.Value.ToString();
-				if (holderName.IsNullOrEmpty())
-				{
-					// If holder name cell cannot be found), it means the row is a sum-up row (i.e., not an legal individual);
-					// therefore we want to skip these rows.
-					continue;
-				}
-
-				if (!sheet.TryGetCell(companyCodeAddress, out Cell companyCodeCell))
-				{
-					System.Console.WriteLine($"Row {rowIndex} has no company name (公司代碼), skip this row.");
-					continue;
-				}
-
-				string companyName = companyCodeCell.Value.ToString();
-				if (companyName.IsNullOrEmpty())
-				{
-					System.Console.WriteLine($"Row {rowIndex} has a null/empty company name (公司代碼), skip this row.");
-					continue;
-				}
-
-				bool isNewCompany = companyName != currentCompanyData.Name;
-				if (isNewCompany) 
-				{
-					if (!currentCompanyData.Name.IsNullOrEmpty())
-					{
-						compantDataCollection.Add(currentCompanyData);
-					}
-
-					// Reset data counters
-					currentCompanyData = new CompanyData();
-					currentCompanyData.Name = companyName;
-
-					System.Console.WriteLine($"-- {companyName} --");
-				}
-
-				// Do various counting.
-
-				currentCompanyData.TotalPositionCount++;
-
-				bool hasPositionCell = sheet.TryGetCell(positionAddress, out Cell positionCell) && !positionCell.Value.ToString().IsNullOrEmpty();
-				if (hasPositionCell) 
-				{
-					string positionName = positionCell.Value.ToString();
-
-					bool isIndependentDirector = positionName.IsIndependentDirectorPosition();
-					if (isIndependentDirector) 
-					{
-						currentCompanyData.IndependentDirectorCount++;
-					}
-
-					bool isBoardMember = positionName.IsBoardMemberPosition();
-					if (isBoardMember)
-					{
-						currentCompanyData.BoardPositionCount++;
-						currentCompanyData.BoardAndIndependentDirectors.Add(holderName.Trim());
-					}
-				}
-
-				bool hasRelativeOrSummaryCell = sheet.TryGetCell(relativeAddress, out Cell relativeCell);
-				if (!hasRelativeOrSummaryCell) 
-				{
-					continue;
-				}
-
-				bool isRelativeCell = relativeCell.DataType == Cell.CellType.STRING && relativeCell.Value.ToString().IsCollectionOfRelatives();
-				if (isRelativeCell) 
-				{
-					string allRelativeNameInput = relativeCell.Value.ToString();
-
-					if (!currentCompanyData.RelativeMap.ContainsKey(holderName)) 
-					{
-						currentCompanyData.RelativeMap.Add(holderName, new HashSet<string>());
-					}
-
-					string[] relativeNamesWithSuffix = allRelativeNameInput.Split(',');
-					foreach (string relativeNameWithSuffix in relativeNamesWithSuffix) 
-					{
-						bool hasValidName = relativeNameWithSuffix.TryExtractRelativeName(out string relativeName);
-						if (!hasValidName) 
-						{
-							continue;
-						}
-
-						currentCompanyData.RelativeMap[holderName].Add(relativeName);
-
-						bool hasLinkBeenRecorded = currentCompanyData.RelativeMap.ContainsKey(relativeName) && 
-												   currentCompanyData.RelativeMap[relativeName].Contains(holderName);
-
-						if (hasLinkBeenRecorded)
-						{
-							System.Console.WriteLine($"{relativeName} - {holderName} (DUPLICATE)");
-							continue;
-						}
-
-						System.Console.WriteLine($"{holderName} - {relativeName}");
-					}
-				}
-			}
-
-			// We do a second pass to calculate board member relative links.
-			foreach (var companyData in compantDataCollection)
-			{
-				// Holder name -> Relative name
-				HashSet<Tuple<string, string>> relativeLinks = new HashSet<Tuple<string, string>>();
-				foreach (var holderNameToRelatives in companyData.RelativeMap)
-				{
-					string holderName = holderNameToRelatives.Key;
-					bool isBoardMember = companyData.BoardAndIndependentDirectors.Contains(holderNameToRelatives.Key);
-
-					foreach (var relativeName in holderNameToRelatives.Value)
-					{
-						Tuple<string, string> holderNameAndRelativeName = new Tuple<string, string>(holderName, relativeName);
-						Tuple<string, string> reversedTuple = new Tuple<string, string>(relativeName, holderName);
-
-						bool hasLinkBeenRecorded = relativeLinks.Contains(reversedTuple) || relativeLinks.Contains(holderNameAndRelativeName);
-						if (hasLinkBeenRecorded)
-						{
-							continue;
-						}
-
-						companyData.RelativeLinkCount++;
-						relativeLinks.Add(holderNameAndRelativeName);
-
-						bool isRelativeBoardMember = companyData.BoardAndIndependentDirectors.Contains(relativeName);
-						if (isBoardMember && isRelativeBoardMember)
-						{
-							companyData.BoardRelativeLinkCount++;
-						}
-					}
-				}	
-			}
-
-			foreach (var companyData in  compantDataCollection)
+			foreach (var companyData in companyDataCollection)
 			{
 				System.Console.WriteLine($"{companyData.Name}: " +
 					$"\t{companyData.TotalPositionCount} 個職位, " +
@@ -214,7 +26,7 @@ namespace RelativeLinkCalculator // Note: actual namespace depends on the projec
 					$"\t{companyData.BoardRelativeLinkCount} 個董事親屬連結");
 			}
 
-			Console.Read();
+			System.Console.Read();
 		}
 	}
 }
